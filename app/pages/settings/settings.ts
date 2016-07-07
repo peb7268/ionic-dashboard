587236f35741289a4ee5fc6ca8d2427e7237965a
peb7268 <peb7268@gmail.com>
2016-07-07 00:33:31 -0400

    initial commit

---
+++ app/pages/settings/settings.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {Platform, ActionSheet, NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/settings/settings.html'
+})
+
+export class SettingsPage {
+  constructor(public platform: Platform, public nav: NavController) {
+  }
+
+  openMenu() {
+    let actionSheet = ActionSheet.create({
+      title: 'Quick Actions',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Remember',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Logout',
+          role: 'logout', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    this.nav.present(actionSheet);
+  }
+}
---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core'
+import {Platform} from 'ionic-angular';
+
+import {HomePage} from '../home/home';
+import {SettingsPage} from '../settings/settings';
+
+@Component({
+  templateUrl: 'build/pages/tabs/tabs.html'
+})
+export class TabsPage {
+
+  private tab1Root: any;
+  private tab2Root: any;
+
+  constructor() {
+    // this tells the tabs component which Pages
+    // should be each tab's root Page
+    this.tab1Root = HomePage;
+    this.tab2Root = SettingsPage;
+  }
+}
